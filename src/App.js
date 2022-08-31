c7496dd96f7cc24f60b081c2456f7bba91b95b11
kanhu-sys <kanhulenka678@gmail.com>
2022-09-01 02:53:14 +0530

    changed basename

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Form from "./Form";
 import Formdata from "./Formdata";
@@ -7,12 +7,12 @@ import Formdata from "./Formdata";
 function App() {
   return (
     <>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename='/patientform'>
         <Routes>
           <Route path='/' element={<Form />} />
           <Route path='form-data' element={<Formdata />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   )
 
