1b84bc64ed5f08e5ab2fc6177be464fe82e21dfd
kanhu-sys <kanhulenka678@gmail.com>
2022-09-01 01:55:21 +0530

    Added Public URL

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Form from "./Form";
 import Formdata from "./Formdata";
 
 
-function App(){
-  return(
+function App() {
+  return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element = {<Form/>}/>
-      <Route path='form-data' element = {<Formdata/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path='/' element={<Form />} />
+          <Route path='form-data' element={<Formdata />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 
